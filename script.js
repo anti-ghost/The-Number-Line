a94cf49186eae85c42e5d3041aa91324ba175bf7
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2022-10-12 19:15:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@
     D(1e12)
   ];
   
-  const CHALLENGE_GOALS = [D(1e12), D(1e20), D(1e16), D(1e18), D(1e52)];
+  const CHALLENGE_GOALS = [D(1e12), D(1e20), D(1e16), D(1e18), D(1e52), D(1e100)];
   
   const MATTER_UPGRADE_COSTS = [D(1e5), D(1e3), D(1e9)];
   
